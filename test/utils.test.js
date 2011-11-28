a3daec815a8e31f4872f19a578367549efaa1f9a
Nathan Landis <my8bird@gmail.com>
2011-11-27 23:04:57 -0600

    all tests are ported over and there does not seem to be any issues running on node 0.6.2

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,15 +3,19 @@
  * Module dependencies.
  */
 
-var knox = require('knox')
-  , utils = knox.utils;
+var knox = require('../index.js'),
+    utils = knox.utils,
+    assert = require('assert');
 
-module.exports = {
-  'test .base64.encode()': function(assert){
+describe('Knox utils', function() {
+  it('test .base64.encode()', function(done) {
     assert.equal('aGV5', utils.base64.encode('hey'));
-  },
+    done()
+  })
   
-  'test .base64.decode()': function(assert){
+  it('test .base64.decode()', function(done) {
     assert.equal('hey', utils.base64.decode('aGV5'));
-  }
-};
+    done();
+  })
+});
+
